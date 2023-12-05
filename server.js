74e72d0fd46bc1c5d9bc1e7607c569edbb8f1a2b
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-12-04 21:36:06 -0800

    made it work

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -13,10 +14,17 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
+
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 
 // LISTEN
 app.listen(PORT, () => {
